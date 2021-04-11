cf4aaf63ba85aa6635183eccb42d020326523858
Xinwei Chai <xinwei.chai@hotmail.com>
2021-04-11 17:53:22 +0800

    visualization

--- TreeNode.py
+++ TreeNode.py
@@ -35,8 +35,30 @@ class TreeNode:
             cur = temp
             ans.append(res)
         print(ans)
-        width = 2 * len(ans[-1]) - 1
-        to_print = [' ' * width for _ in range(2 * len(ans) - 1)]
+
+        ans = [[str(j) if j is not None else ' ' for j in i] for i in ans]
+        # for i in range(len(ans) - 2, -1, -1):
+        #     for j in range(len(ans[i])):
+        #         ans[i][j] = ' ' * ((len(ans[i + 1][j * 2]) + len(ans[i + 1][j * 2])) // 2 - len(ans[i][j])) + ans[i][j] + ' ' * ((len(ans[i + 1][j * 2]) + len(ans[i + 1][j * 2])) // 2 - len(ans[i][j]))
+        # res = [' ' * (2 ** (len(ans) - i - 1) - 1) + (' ' * (2 ** (len(ans) - i) - 1)).join(ans[i]) + ' ' * (
+        #         2 ** (len(ans) - i - 1) - 1) for i in range(len(ans))]
+        # for i in res:
+        #     print(i)
+
+        for i in range(len(ans) - 2, -1, -1):
+            for j in range(len(ans[i])):
+                left = ' ' * (len(ans[i + 1][j * 2]) - len(ans[i][j]))
+                right = ' ' * (len(ans[i + 1][j * 2 + 1]) - len(ans[i][j]))
+                ans[i][j] = left + ans[i][j] + right
+
+        for i in range(len(ans)-1):
+            for j in range(len(ans[i])):
+                left = ' ' * (2 ** (len(ans) - 1 - i) - 1)
+                right = (2 ** (len(ans) - 1 - i) - 1)
+                ans[i][j] = left + ans[i][j] + ' ' * right
+
+        for i in ans:
+            print(' '.join(i))
 
 
 def create_tree(l: List[List]):
@@ -46,12 +68,12 @@ def create_tree(l: List[List]):
         cnt = 0
         temp = []
         for j in cur:
-            if not j:
+            if j is None:
                 cnt += 2
                 temp.append(None)
                 temp.append(None)
                 continue
-            if i[cnt]:
+            if i[cnt] is not None:
                 j.left = TreeNode(i[cnt])
             temp.append(j.left)
             cnt += 1
@@ -61,3 +83,6 @@ def create_tree(l: List[List]):
             cnt += 1
         cur = temp
     return root
+
+
+create_tree([[1], [2, 3], [4, 5, 6, 7], [8, 9, 10, 11, 12, 13, 14, 15]]).print_all()
