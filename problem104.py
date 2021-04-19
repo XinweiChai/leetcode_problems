49034b02293e676087ab431ee1e4cdd78a513493
Xinwei Chai <xinwei.chai@hotmail.com>
2021-04-19 08:44:33 +0800

    reorganize

--- problem104.py
+++
@@ -1,8 +0,0 @@
-from TreeNode import TreeNode
-
-
-class Solution(object):
-    def maxDepth(self, root: TreeNode) -> int:
-        if not root:
-            return 0
-        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
--- problem7.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def reverse(self, x: int) -> int:
-        s = (x > 0) - (x < 0)
-        r = int(str(x*s)[::-1])
-        return s*r * (r < 2**31)
