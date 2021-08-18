cd187c148e0c4576138425ccf5102f58d237a802
Xinwei Chai <xinwei.chai@hotmail.com>
2021-08-18 17:52:52 +0800

    one hard problem

---
+++ python/problem446.py
@@ -0,0 +1,49 @@
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        n = len(nums)
+        f = [{} for _ in range(n)]  # map from (i, d) to freq
+        cnt = 0
+        for i in range(1, n):
+            for j in range(i):
+                diff = nums[i] - nums[j]
+                cnt += f[j].get(diff, 0)
+                f[i][diff] = f[i].get(diff, 0) + f[j].get(diff, 0) + 1
+        return cnt
+
+    def numberOfArithmeticSlices2(self, nums: List[int]) -> int:
+        positions = defaultdict(list)
+        n = len(nums) - 1
+        for i, a in enumerate(reversed(nums)):
+            positions[a].append(n - i)
+        previous = defaultdict(int)
+        subseqs = [defaultdict(int) for _ in range(len(nums))]
+        for i, a in enumerate(nums):
+            del positions[a][-1]
+            if not positions[a]:
+                del positions[a]
+            if len(positions) > len(previous):
+                for b in previous:
+                    c = (a << 1) - b
+                    if c in positions:
+                        n = previous[b] + subseqs[i][b]
+                        for j in positions[c]:
+                            if j <= i:
+                                break
+                            subseqs[j][a] += n
+            else:
+                for c in positions:
+                    b = (a << 1) - c
+                    if b in previous:
+                        n = previous[b] + subseqs[i][b]
+                        for j in positions[c]:
+                            subseqs[j][a] += n
+            previous[a] += 1
+        return sum(sum(p.values()) for p in subseqs)
+
+
+if __name__ == '__main__':
+    print(Solution().numberOfArithmeticSlices2([1, 1, 2, 3, 4, 5]))
---
+++ python/problem447.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def numberOfBoomerangs(self, points: List[List[int]]) -> int:
+        cnt = 0
+        for x1, y1 in points:
+            lookup = {}
+            for x2, y2 in points:
+                if x1 == x2 and y1 == y2:
+                    continue
+                dx, dy = x1 - x2, y1 - y2
+                d = dx * dx + dy * dy
+                if d in lookup:
+                    cnt += lookup[d]
+                    lookup[d] += 1
+                else:
+                    lookup[d] = 1
+        return 2 * cnt
+
+
+if __name__ == '__main__':
+    print(Solution().numberOfBoomerangs([[0, 0], [1, 0], [2, 0]]))
