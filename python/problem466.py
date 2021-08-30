f6cde1b5bbc5b79b9dec5cb8fe1f3b056a8200d4
Xinwei Chai <xinwei.chai@hotmail.com>
2021-08-30 15:48:26 +0800

    a hard problem

--- python/problem466.py
+++ python/problem466.py
@@ -1,47 +1,55 @@
 class Solution:
     def getMaxRepetitions(self, s1: str, n1: int, s2: str, n2: int) -> int:
-        try:
-            pivot = s1.index(s2[0])
-        except ValueError:
-            return 0
-        ptr1 = pivot + 1
-        ptr2 = 1
-        cnt1 = 1
-        cnt2 = 1
-        while ptr1 != len(s1) or ptr2 != len(s2):
+        pivots = {}
+        ptr1 = ptr2 = 0
+        cnt1 = cnt2 = 0
+        last = 0
+        while 1:
             if ptr2 == len(s2):
                 cnt2 += 1
                 ptr2 = 0
-            if ptr1 == len(s1):
-                ptr1 = 0
-                cnt1 += 1
             try:
-                ptr1 = s1.index(s2[ptr2], ptr1) + 1
+                ptr1 = s1.index(s2[ptr2], ptr1)
+                if ptr2 == 0:
+                    if ptr1 in pivots:
+                        break
+                    else:
+                        last = cnt2
+                        pivots[ptr1] = (cnt1, cnt2)
+                ptr1 += 1
                 ptr2 += 1
             except ValueError:
-                if not cnt1:
+                if ptr1 == 0:
                     return 0
-                ptr1 = len(s1)
-                if ptr2 == 0:
-                    break
-        return n1 // cnt1 * cnt2 // n2 + self.residual(s1, n1 % cnt1, s2, n2)
+                cnt1 += 1
+                if cnt1 > n1:
+                    return last // n2
+                ptr1 = 0
+        last1, last2 = pivots[ptr1]
+        # flag is for the cases when a pattern does not begin at the beginning of s1
+        flag = ptr1 != 0
+        repeat, rest = divmod(n1 - last1 - flag, cnt1 - last1)
+        return (last2 + repeat * (cnt2 - last2) + self.residual(s1, rest + flag, s2, ptr1)) // n2
 
-    def residual(self, s1: str, n1: int, s2: str, n2: int):
-        pivot = s1.index(s2[0])
+    def residual(self, s1: str, n1: int, s2: str, pivot):
         ptr1 = pivot + 1
         ptr2 = 1
         cnt2 = 0
-        while ptr1 != len(s1):
+        while n1:
             if ptr2 == len(s2):
                 cnt2 += 1
                 ptr2 = 0
             try:
                 ptr1 = s1.index(s2[ptr2], ptr1) + 1
             except ValueError:
-                ptr1 = len(s1)
+                ptr1 = 0
+                n1 -= 1
             ptr2 += 1
         return cnt2
 
 
 if __name__ == '__main__':
     print(Solution().getMaxRepetitions(s1="baba", n1=11, s2="baab", n2=1))
+    print(Solution().getMaxRepetitions("lovelivenanjomusicforever", 100000, "nanjo", 10))
+    print(Solution().getMaxRepetitions("aaa", 3, "aa", 1))
+    print(Solution().getMaxRepetitions("bacaba", 3, "abacab", 1))
