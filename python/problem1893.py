2f5646b621bc1f63d70275b660631b6605c96b0b
Xinwei Chai <xinwei.chai@hotmail.com>
2021-10-08 17:32:49 +0800

    interesting problem

---
+++ python/problem1893.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
+        ranges.sort()
+        if ranges[0][0] > left:
+            return False
+        p = 0
+        r = ranges[p][1]
+        while p < len(ranges):
+            if left < r and ranges[p][0] > r + 1:
+                return False
+            r = max(r, ranges[p][1])
+            if r >= right:
+                return True
+            p += 1
+        return False
